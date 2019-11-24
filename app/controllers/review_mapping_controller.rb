a7c9d29a4b7e8bbeb86f6c439ba4b8214d69838b
mtrawic <mtrawic@ncsu.edu>
2019-11-24 18:51:36 -0500

    Trying to get reviews working

--- app/controllers/review_mapping_controller.rb
+++ app/controllers/review_mapping_controller.rb
@@ -85,7 +85,8 @@ class ReviewMappingController < ApplicationController
   def assign_reviewer_dynamically
     assignment = Assignment.find(params[:assignment_id])
     reviewer = AssignmentParticipant.where(user_id: params[:reviewer_id], parent_id: assignment.id).first
-
+    puts  "assignment = #{assignment}"
+    puts  "reviewer = #{reviewer}"
     if params[:i_dont_care].nil? && params[:topic_id].nil? && assignment.topics? && assignment.can_choose_topic_to_review?
       flash[:error] = "No topic is selected.  Please go back and select a topic."
     else
