1605f30945f75bfb99cd50198870b68b87fe77f4
samruddhikhandale <khandale.samruddhi@gmail.com>
2019-11-29 22:52:03 -0500

    Edit function

--- app/models/assignment_form.rb
+++ app/models/assignment_form.rb
@@ -353,7 +353,7 @@ class AssignmentForm
 
   def is_instructor_a_participant?(instructor_id)
       if @assignment.id != nil
-        @is_instructor_a_participant = Participant.where("user_id LIKE '#{instructor_id_id}' and parent_id LIKE '#{@assignment.id}' " )
+        @is_instructor_a_participant = Participant.where("user_id LIKE '#{instructor_id}' and parent_id LIKE '#{@assignment.id}' " )
         if @is_instructor_a_participant.present?
           return true
         end
