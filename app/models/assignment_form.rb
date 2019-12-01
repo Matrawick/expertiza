c798f833816cf93d1a352ec60bd009b1f3e9d040
samruddhikhandale <khandale.samruddhi@gmail.com>
2019-11-30 20:18:36 -0500

    Delete

--- app/models/assignment_form.rb
+++ app/models/assignment_form.rb
@@ -354,7 +354,8 @@ class AssignmentForm
   # This method returns true if the instructor is already added as a participant to the assignment which he/she is editing
   def is_instructor_a_participant?(instructor_id)
       if @assignment.id != nil
-        @is_instructor_a_participant = Participant.where("user_id LIKE '#{instructor_id}' and parent_id LIKE '#{@assignment.id}' " )
+        puts 'IAss id : ' + @assignment.id
+        @is_instructor_a_participant = Participant.where(user_id: instructor_id , parent_id: @assignment.id)
         puts 'Instructor name :' + @is_instructor_a_participant.parent_id
         if !@is_instructor_a_participant.blank?
           return true
