0271329fce5ccfaa213fa62c243bd6ac23f53922
samruddhikhandale <khandale.samruddhi@gmail.com>
2019-11-30 21:46:59 -0500

    Delete

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -180,10 +180,11 @@ class AssignmentsController < ApplicationController
 
 
   def delete_instructor_as_participant(assignment_id , instructor_id)
-    #@participant =
-    #participant = Participant.find(params[:id])
+
+    participant = Participant.where(user_id: instructor_id , parent_id: assignment_id)
+
     begin
-      Participant.where(user_id: instructor_id , parent_id: assignment_id).delete(params[:force])
+      participant.destroy
       #flash[:note] = undo_link("The user \"#{participant.user.name}\" has been successfully removed as a participant.")
     rescue StandardError
       #flash[:error] = 'The delete action failed: At least one review mapping or team membership exist for this participant.'
