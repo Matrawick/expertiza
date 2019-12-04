2c14f76f91ecf55efaf3f99ff6e7b532d2ed7cee
samruddhikhandale <khandale.samruddhi@gmail.com>
2019-12-03 23:16:12 -0500

    Rspec 1

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -155,14 +155,8 @@ describe AssignmentsController do
     context 'when "Add yourself as a participant?" checkbox is selected' do
       it 'adds instructor as a participant to the newly created assignment' do
         allow(assignment_form).to receive(:assignment).and_return(assignment)
-        allow(assignment_form).to receive(:is_instructor_a_participant).with(any_args).and_return(false)
-        is_instructor_participant = false
-        add_instructor = 1
-        if add_instructor == 1 and is_instructor_participant == false
-          allow(assignment_form).to receive(:add_participant).with(any_args).and_return(true)
-        end
-        post :create, @params
-
+        allow(assignment_form).to receive(:add_participant).with(any_args).and_return(true)
+        post :add_participant
       end
     end
   end
