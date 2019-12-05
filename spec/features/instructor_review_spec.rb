9e94341ccbc9394477622ae19dcbbbf5ece977b7
mtrawic <mtrawic@ncsu.edu>
2019-12-05 13:18:01 -0500

    Editing test from prev group

---
+++ spec/features/instructor_review_spec.rb
@@ -0,0 +1,34 @@
+describe "instructor review testing" do
+  before(:each) do
+    create(:assignment, name: "TestAssignment", directory_path: 'test_assignment')
+    create_list(:participant, 3)
+    create(:assignment_node)
+    create(:deadline_type, name: "submission")
+    create(:deadline_type, name: "review")
+    create(:deadline_type, name: "metareview")
+    create(:deadline_type, name: "drop_topic")
+    create(:deadline_type, name: "signup")
+    create(:deadline_type, name: "team_formation")
+    create(:deadline_right)
+    create(:deadline_right, name: 'Late')
+    create(:deadline_right, name: 'OK')
+    create(:assignment_due_date, deadline_type: DeadlineType.where(name: 'review').first, due_at: Time.now.in_time_zone + 1.day)
+    create(:topic)
+    create(:topic, topic_name: "TestReview")
+    create(:team_user, user: User.where(role_id: 2).first)
+    create(:team_user, user: User.where(role_id: 2).second)
+    create(:assignment_team)
+    create(:team_user, user: User.where(role_id: 2).third, team: AssignmentTeam.second)
+    create(:signed_up_team)
+    create(:signed_up_team, team_id: 2, topic: SignUpTopic.second)
+    create(:assignment_questionnaire)
+    create(:question)
+    create(:review_response_map, reviewer_id: User.find_by(name: "instructor6").id, reviewee: AssignmentTeam.second)
+  end
+
+  it "lets instructor perform a review" do
+    assignment = Assignment.first
+    login_as("instructor6")
+    visit "/assignments/list_submissions?id=#{assignment.id}"
+    expect(page).to have_content 'Begin Review'
+  end
